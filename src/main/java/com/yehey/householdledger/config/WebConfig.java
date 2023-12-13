da87633cecf3b7f1cf14ed845a914f7dad74d889
park ye jin <dyddyd0514@naver.com>
2023-12-13 14:08:50 +0900

    [Add] CORS allow localhost

---
+++ src/main/java/com/yehey/householdledger/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.yehey.householdledger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://127.0.0.1:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}

c6372327acaab47e454b24071bb4c6638faf2761
park ye jin <dyddyd0514@naver.com>
2023-12-21 14:59:25 +0900

    [Feat] get total amount value from ledger

--- src/main/java/com/yehey/householdledger/repository/LedgerRepository.java
+++ src/main/java/com/yehey/householdledger/repository/LedgerRepository.java
@@ -2,6 +2,8 @@ package com.yehey.householdledger.repository;
 
 import com.yehey.householdledger.entity.Ledger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.List;
 public interface LedgerRepository extends JpaRepository<Ledger,Long> {
     List<Ledger> getAllByDateBetweenOrderByDate(LocalDate start, LocalDate end);
 
+    @Query(value = "select sum(l.amount) as total from Ledger l where l.archiveTypeID.archiveTypeID=:archiveTypeID and l.date>:start and l.date<:end")
+    Long getTotalSumByDateAndArchiveTypeID(@Param(value = "archiveTypeID")Long archiveTypeID,@Param(value="start") LocalDate start, @Param(value = "end") LocalDate end);
 }

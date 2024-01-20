b00c501c59c8a57cd96eaf621ad911c52d9c0db5
park ye jin <dyddyd0514@naver.com>
2024-01-20 17:33:42 +0900

    [Feat] get all withdrawal List api

--- src/main/java/com/yehey/householdledger/controller/WithdrawalController.java
+++ src/main/java/com/yehey/householdledger/controller/WithdrawalController.java
@@ -1,11 +1,13 @@
 package com.yehey.householdledger.controller;
 
 
+import com.yehey.householdledger.dto.response.DataResponseDTO;
 import com.yehey.householdledger.dto.response.ResponseDTO;
 import com.yehey.householdledger.dto.withdrawal.PostWithdrawalRequestDTO;
 import com.yehey.householdledger.service.WithdrawalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class WithdrawalController {
-    private final WithdrawalService withdrawalService;
 
-    @PostMapping("")
-    public ResponseDTO postWithdrawal(
-            @RequestBody PostWithdrawalRequestDTO dto
-    ){
-        withdrawalService.saveWithdrawal(dto);
-        return ResponseDTO.of("success",0,"create withdrawal success");
-    }
+  private final WithdrawalService withdrawalService;
+
+  @PostMapping("")
+  public ResponseDTO postWithdrawal(
+      @RequestBody PostWithdrawalRequestDTO dto
+  ) {
+    withdrawalService.saveWithdrawal(dto);
+    return ResponseDTO.of("success", 0, "create withdrawal success");
+  }
+
+  @GetMapping("")
+  public ResponseDTO getAllWithdrawal() {
+    return DataResponseDTO.of(withdrawalService.getAllWithdrawalList(), "get all withdrawal list");
+  }
 
 }
--- src/main/java/com/yehey/householdledger/service/WithdrawalService.java
+++ src/main/java/com/yehey/householdledger/service/WithdrawalService.java
@@ -1,6 +1,8 @@
 package com.yehey.householdledger.service;
 
+import com.yehey.householdledger.dto.tag.TagResponseDTO;
 import com.yehey.householdledger.dto.withdrawal.PostWithdrawalRequestDTO;
+import com.yehey.householdledger.dto.withdrawal.WithdrawalResponseDTO;
 import com.yehey.householdledger.entity.ArchiveType;
 import com.yehey.householdledger.entity.AutomaticWithdrawal;
 import com.yehey.householdledger.entity.Tag;
@@ -10,34 +12,76 @@ import com.yehey.householdledger.repository.TagRepository;
 import com.yehey.householdledger.repository.WithdrawalRepostiory;
 import com.yehey.householdledger.repository.WithdrawalTagRelataionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WithdrawalService {
-    private final ArchiveTypeRepository archiveTypeRepository;
-    private final TagRepository tagRepository;
-    private final WithdrawalRepostiory withdrawalRepostiory;
-    private final WithdrawalTagRelataionRepository withdrawalTagRelataionRepository;
 
-    public void saveWithdrawal(PostWithdrawalRequestDTO dto){
-        ArchiveType archiveType = archiveTypeRepository.findByArchiveTypeID(dto.getArchiveTypeID());
+  private final ArchiveTypeRepository archiveTypeRepository;
+  private final TagRepository tagRepository;
+  private final WithdrawalRepostiory withdrawalRepostiory;
+  private final WithdrawalTagRelataionRepository withdrawalTagRelataionRepository;
 
-        AutomaticWithdrawal newWithdrawal = AutomaticWithdrawal.builder()
-                .amount(dto.getAmount())
-                .archiveTypeID(archiveType)
-                .cycle(dto.getCycle())
-                .isExcluded(dto.getIsExcluded())
-                .memo(dto.getMemo())
-                .title(dto.getTitle())
-                .build();
+  public void saveWithdrawal(PostWithdrawalRequestDTO dto) {
+    ArchiveType archiveType = archiveTypeRepository.findByArchiveTypeID(dto.getArchiveTypeID());
 
-        withdrawalRepostiory.save(newWithdrawal);
+    AutomaticWithdrawal newWithdrawal = AutomaticWithdrawal.builder()
+        .amount(dto.getAmount())
+        .archiveTypeID(archiveType)
+        .cycle(dto.getCycle())
+        .isExcluded(dto.getIsExcluded())
+        .memo(dto.getMemo())
+        .title(dto.getTitle())
+        .build();
 
-        for (Long tagID:dto.getTagList()){
-            Tag tag = tagRepository.findByTagID(tagID);
-            withdrawalTagRelataionRepository.save(WithdrawalTagRelation.builder().tag(tag).withdrawal(newWithdrawal).build());
-        }
+    withdrawalRepostiory.save(newWithdrawal);
 
+    for (Long tagID : dto.getTagList()) {
+      Tag tag = tagRepository.findByTagID(tagID);
+      withdrawalTagRelataionRepository.save(
+          WithdrawalTagRelation.builder().tag(tag).withdrawal(newWithdrawal).build());
     }
+
+  }
+
+  public List<TagResponseDTO> getRelatedTagList(AutomaticWithdrawal withdrawal) {
+    List<TagResponseDTO> tagResponseDTOList = new ArrayList<>();
+
+    for (WithdrawalTagRelation relation : withdrawal.getRelation()) {
+      Tag tag = relation.getTag();
+      TagResponseDTO tagged = TagResponseDTO.builder().name(tag.getName()).tagID(tag.getTagID())
+          .build();
+      tagResponseDTOList.add(tagged);
+    }
+
+    return tagResponseDTOList;
+  }
+
+  public List<WithdrawalResponseDTO> getAllWithdrawalList() {
+    List<AutomaticWithdrawal> withdrawalList = withdrawalRepostiory.findAll(
+        Sort.by(Sort.Direction.ASC, "cycle"));
+    List<WithdrawalResponseDTO> withdrawalResponseDTOList = new ArrayList<>();
+
+    for (AutomaticWithdrawal withdrawal : withdrawalList) {
+      List<TagResponseDTO> tagResponseDTOList = getRelatedTagList(withdrawal);
+
+      withdrawalResponseDTOList.add(WithdrawalResponseDTO.builder()
+          .withdrawalID(withdrawal.getWithdrawalID())
+          .cycle(withdrawal.getCycle())
+          .amount(withdrawal.getAmount())
+          .memo(withdrawal.getMemo())
+          .isExcluded(withdrawal.getIsExcluded())
+          .title(withdrawal.getTitle())
+          .archiveType(withdrawal.getArchiveTypeID())
+          .tagList(tagResponseDTOList)
+          .build());
+    }
+
+    return withdrawalResponseDTOList;
+  }
 }
